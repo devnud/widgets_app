d83aba773077e321bc5413918a9ca6f576f33aed
devnud <taximadridavid@gmail.com>
2023-12-21 18:23:35 +0100

    agragamos menu_items

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart' show IconData, Icons;
+
+class MenuItems {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItems({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItems>[
+  MenuItems(
+    title: 'Botones',
+    subTitle: 'Varios tipos de botones',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined,
+  ),
+  MenuItems(
+    title: 'Tarjetas',
+    subTitle: 'Un contenedor estilizado',
+    link: '/card',
+    icon: Icons.credit_card,
+  ),
+];

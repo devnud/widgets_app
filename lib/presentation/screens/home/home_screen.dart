9a1c520e540a6fa253c8ef3ea937d8ee9fbf145a
devnud <taximadridavid@gmail.com>
2023-12-21 22:42:49 +0100

    rutas con mombre implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-
+import 'package:widgets_app/presentation/screens/screens.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
@@ -61,8 +63,9 @@ class _CustomListTile extends StatelessWidget {
         //   ),
         // );
         // Navigator.of(context).pushNamed(item.link);
-        context.push(item.link);
 
+        // context.pushNamed(CardsScreen.name);
+        context.push(item.link);
       },
     );
   }

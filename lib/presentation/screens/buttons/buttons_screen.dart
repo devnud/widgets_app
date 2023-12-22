7e31efcddf91cccbc9829832916748764eb28b33
devnud <taximadridavid@gmail.com>
2023-12-22 13:02:00 +0100

    Buttons screen finist

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -30,7 +30,6 @@ class _ButtonsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
     return SizedBox(
       width: double.infinity,
@@ -96,9 +95,30 @@ class _ButtonsView extends StatelessWidget {
                 iconColor: const MaterialStatePropertyAll(Colors.white),
               ),
             ),
-           
-
+            const CustomButton(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+     final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+            child: Text('Hola Mundo', style: TextStyle(color: Colors.white),)),
         ),
       ),
     );

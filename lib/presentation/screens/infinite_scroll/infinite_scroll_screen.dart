78d9d40a450e139592630971f9ed2007ac7684f6
devnud <taximadridavid@gmail.com>
2023-12-23 12:37:05 +0100

    implementando isLoading

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -11,6 +12,53 @@ class InfiniteScrollScreen extends StatefulWidget {
 
 class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
   List<int> imagesIds = [1, 2, 3, 4, 5];
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        loadNextPage();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false;
+    super.dispose();
+  }
+
+  Future loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    addFiveImages();
+    isLoading = false;
+
+    if (!isMounted) return;
+
+    setState(() {
+      // Todo: mover scroll
+    });
+  }
+
+  void addFiveImages() {
+    final lasId = imagesIds.last;
+    imagesIds.addAll(
+      [1, 2, 3, 4, 5].map((e) => lasId + e),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +68,7 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
+          controller: scrollController,
           itemCount: imagesIds.length,
           itemBuilder: (BuildContext context, int index) {
             return FadeInImage(
@@ -35,7 +84,13 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => context.pop(),
-        child: const Icon(Icons.arrow_back_ios_new_outlined),
+      
+      child: isLoading
+          ? SpinPerfect(
+              infinite: true,
+              child: const Icon(Icons.refresh_rounded),
+            )
+          : FadeIn(child: const Icon(Icons.arrow_back_ios_new_outlined)),
       ),
     );
   }
